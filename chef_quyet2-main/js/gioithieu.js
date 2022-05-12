594d534d67b5544c394bd506af1deb57c95fc853
HienHy <hiennguyenhy0911@gmail.com>
2022-05-12 23:34:29 +0700

    Initial commit

---
+++ chef_quyet2-main/js/gioithieu.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $(".icon-messenger").click(function() {
+        $(".chat-popup").toggleClass("hidden");
+    });
+
+    $(".close-icon").click(function() {
+        $(".chat-popup").toggleClass("hidden");
+    });
+  });
+  $(document).ready(function() {
+    $(".icon-cart").click(function() {
+        $(".chat-popup-cart").toggleClass("hidden");
+        $('body').addClass('body-background');
+    });
+
+    $(".close-icon-cart").click(function() {
+        $(".chat-popup-cart").toggleClass("hidden");
+        $('body').removeClass('body-background');
+    });
+  });
+$(window).load(function () {
+    $('#preloader').fadeOut(2000, function () {
+        $(this).remove();
+    });
+});
